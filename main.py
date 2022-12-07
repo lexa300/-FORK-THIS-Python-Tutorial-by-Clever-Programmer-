c54336b05eee321ca339a5af497a4fc5a116b030
Lexa 'Yolanda' Pervard <>
2022-12-07 19:06:33 +0000

    Tip Calculator App

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def factorial(n):
-  if n == 0:
-    return 1
-  else:
-    return n * factorial(n - 1)
-print(factorial(5))
+#Tip Calculator App 
+
+food_amount = float(input('Enter food amount $:'))
+tip_percentage = float(input('Enter your tip percentage %:')) /100
+tip_amount = food_amount * tip_percentage
+
+# Total = food_amount + tip_amount
+total = food_amount + tip_amount
+print('\n\n\n')
+print('-------------------------')
+print(f' Food Amount: ${food_amount}')
+print(f' Tip Amount : ${tip_amount}')
+print('\n')
+print(f' Total Amount: ${total}')
+print('-------------------------')
+
